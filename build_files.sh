add8c9ef7dca15eb5f07044a7111f32430e6a8dc
Tedi Sina <81098784+TediSina@users.noreply.github.com>
2025-05-03 21:25:49 +0200

    Get database info from env.

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py tailwind install
+python3.12 manage.py migrate
 python3.12 manage.py tailwind build
 python3.12 manage.py collectstatic --noinput
