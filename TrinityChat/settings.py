cc99f46fb91fb71a3548dd5a70f57c8100ebfc6d
Tedi Sina <81098784+TediSina@users.noreply.github.com>
2025-05-03 22:29:13 +0200

    Update settings.py

--- TrinityChat/settings.py
+++ TrinityChat/settings.py
@@ -36,12 +36,12 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
