c642512ee1ea91c6b8cb890cab7962808582e2ca
Tedi Sina <81098784+TediSina@users.noreply.github.com>
2025-05-03 22:24:33 +0200

    Update settings.py

--- TrinityChat/settings.py
+++ TrinityChat/settings.py
@@ -161,15 +161,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
