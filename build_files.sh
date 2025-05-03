f30d498a4fd7058ef4a5d35d35460c63273a1465
Tedi Sina <81098784+TediSina@users.noreply.github.com>
2025-05-03 18:38:45 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py tailwind install
-python3.12 manage.py migrate
 python3.12 manage.py tailwind build
 python3.12 manage.py collectstatic --noinput
